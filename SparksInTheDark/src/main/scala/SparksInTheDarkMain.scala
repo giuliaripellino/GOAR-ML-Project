2d7a5885d555de6dab83cca9683e247e3f7e16a3
axelgallen <axel.gallen@physics.uu.se>
2024-02-23 14:38:09 +0100

    Changed paths to signal and bkg in bucket

--- SparksInTheDark/src/main/scala/SparksInTheDarkMain.scala
+++ SparksInTheDark/src/main/scala/SparksInTheDarkMain.scala
@@ -10,13 +10,13 @@ object SparksInTheDarkMain {
 
     // Read in data from parquet
     val df_background = spark.read
-      .parquet("data/ntuple_em_v2.parquet")
+      .parquet("gs://sitd-parquet-bucket/ntuple_em_v2.parquet")
 
     df_background.show()
     df_background.printSchema()
 
     val df_signal = spark.read
-      .parquet("data/ntuple_SU2L_25_500_v2.parquet")
+      .parquet("gs://sitd-parquet-bucket/ntuple_SU2L_25_500_v2.parquet")
 
     df_signal.show()
     df_signal.printSchema()
