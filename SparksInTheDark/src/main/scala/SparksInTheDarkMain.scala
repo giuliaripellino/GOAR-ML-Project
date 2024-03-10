7387b30a359d25e88f0c7a104a588cb0a1679355
axelgallen <axel.gallen@physics.uu.se>
2024-03-10 15:59:37 +0000

    Added function to convert dfs to RDDs

--- SparksInTheDark/src/main/scala/SparksInTheDarkMain.scala
+++ SparksInTheDark/src/main/scala/SparksInTheDarkMain.scala
@@ -1,4 +1,8 @@
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.random.RandomRDDs.normalVectorRDD
+import org.apache.spark.mllib.linalg.{Vectors, Vector => MLVector, _}
+import org.apache.spark.sql.{DataFrame, Row, SQLContext, SQLImplicits, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
 object SparksInTheDarkMain {
   def main(args: Array[String]): Unit = {
     println("Starting SparkSession...")
@@ -11,14 +15,14 @@ object SparksInTheDarkMain {
 
     // Read in data from parquet
     val df_background = spark.read
-      .parquet("gs://sitd-parquet-bucket/ntuple_em_v2.parquet")
-    
+      //.parquet("gs://sitd-parquet-bucket/ntuple_em_v2.parquet")
+      .parquet("data/ntuple_em_v2.parquet")
     df_background.show()
 
     // WRONG SIGNAL SAMPLE
     val df_signal = spark.read
-      .parquet("gs://sitd-parquet-bucket/ntuple_SU2L_25_500_v2.parquet")
-
+      //.parquet("gs://sitd-parquet-bucket/ntuple_SU2L_25_500_v2.parquet")
+      .parquet("data/ntuple_SU2L_25_500_v2.parquet")
     df_signal.show()
 
     // Function which filters based on pre-defined pre-selection & selects the interesting variables
@@ -44,8 +48,38 @@ object SparksInTheDarkMain {
     println(s"# Background events after filter: ${filtered_bkg_count}")
     println(s"# Signal events before filter: ${original_signal_count}")
     println(s"# Signal events after filter: ${filtered_signal_count}")
+    val trainSize : Long = math.pow(10, 3).toLong
+    val trainingRDD : RDD[MLVector] = normalVectorRDD(spark.sparkContext, trainSize, 4, 3, 1230568)
+    trainingRDD.take(10).foreach(println)
 
-    // Create histogram as outlined in SparkDensityTree-Introduction.scala
+    // Turn spark dataframes into RDD
+    def df_to_RDD(df: DataFrame): org.apache.spark.rdd.RDD[org.apache.spark.mllib.linalg.Vector]  = {
+      df.rdd.map {row =>
+        val deltaRLep2ndClosestBJet = row.getAs[Float]("deltaRLep2ndClosestBJet").toDouble
+        val lJet_m_plus_RCJet_m_12 = row.getAs[Float]("LJet_m_plus_RCJet_m_12").toDouble
+        val bb_m_for_minDeltaR = row.getAs[Float]("bb_m_for_minDeltaR").toDouble
+        Vectors.dense(deltaRLep2ndClosestBJet, lJet_m_plus_RCJet_m_12, bb_m_for_minDeltaR)
+        }
+      }
+    val backgroundRDD = df_to_RDD(filtered_background)
+    //val backgroundRDD = filtered_background.rdd.map { row =>
+    //  val deltaRLep2ndClosestBJet = row.getAs[Float]("deltaRLep2ndClosestBJet").toDouble
+    //  val lJet_m_plus_RCJet_m_12 = row.getAs[Float]("LJet_m_plus_RCJet_m_12").toDouble
+    //  val bb_m_for_minDeltaR = row.getAs[Float]("bb_m_for_minDeltaR").toDouble
+    //  Vectors.dense(deltaRLep2ndClosestBJet, lJet_m_plus_RCJet_m_12, bb_m_for_minDeltaR)}
+
+    println("Showing the first 10 RDD vectors")
+    backgroundRDD.take(10).foreach(println)
+
+    // Turn RDD into Minimum Density Estimate Histograms (mdeHists)
+
+
+    // Plot mdeHists
+
+    // Get 10% highest density regions
+
+    // Plot 10% highest density regions
+
 
     // Write out csv file on the form 
     // deltaR_min_bb	<	2.7
