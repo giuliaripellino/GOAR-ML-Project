9dd8209b01957c54afd728ec7bce0becc6dec800
axelgallen <axel.gallen@physics.uu.se>
2024-03-15 16:29:44 +0100

    First implementation of n:d scatterplots

--- Postprocessing/plotting.py
+++ Postprocessing/plotting.py
@@ -1,6 +1,5 @@
 from matplotlib import cm
 import matplotlib.pyplot as plt
-#import matplotlib_inline.backend_inline
 import numpy as np
 from pyspark.sql import SparkSession
 from matplotlib import cbook
@@ -9,27 +8,20 @@ from matplotlib.ticker import LinearLocator
 from matplotlib.patches import Rectangle, PathPatch
 import mpl_toolkits.mplot3d.art3d as art3d
 from matplotlib.transforms import Bbox
-
-# Create a SparkSession
-spark = SparkSession.builder \
-    .appName("Read Parquet File") \
-    .getOrCreate()
+import pandas as pd
 
 limitsPath = "/Users/axega337/Documents/PhD/SparksInTheDark/GOAR-ML-Project/SparksInTheDark/output/output_3d/limits/"
 valuesPath = "/Users/axega337/Documents/PhD/SparksInTheDark/GOAR-ML-Project/SparksInTheDark/output/output_3d/plotValues/"
-
 samplePath = "/Users/axega337/Documents/PhD/SparksInTheDark/GOAR-ML-Project/SparksInTheDark/output/output_3d/sample/"
 
 def plotDensity(pointsPerAxis, z_max, limitsPath, valuesPath):
 
-    #matplotlib_inline.backend_inline.set_matplotlib_formats('png2x')
+    limits = np.array(pd.read_parquet(limitsPath))[-1,-1]
+    values = np.array(pd.read_parquet(valuesPath))[-1,-1]
 
-    limits = np.array(spark.read.parquet(limitsPath).collect())[-1,-1]
-    values = np.array(spark.read.parquet(valuesPath).collect())[-1,-1]
+    for i in range(0, len(limits), 2):
+        print(f"Variable {i//2 + 1} limits: ({limits[i]}, {limits[i+1]})")
 
-    print(limits)
-
-    print(values)
     x4_min = limits[0]
     x4_max = limits[1]
     x6_min = limits[2]
@@ -65,19 +57,17 @@ def plotDensity(pointsPerAxis, z_max, limitsPath, valuesPath):
     plt.show()
 
 
-plotDensity(256,0.0002, limitsPath, valuesPath)
+#plotDensity(256,0.0002, limitsPath, valuesPath)
 
 def scatterPlot(dimensions, alph, limitsPath, samplePath):
 
-    limits = np.array(spark.read.parquet(limitsPath).collect())[-1,-1]
-    values = np.array(spark.read.parquet(samplePath).collect())[-1,-1]
-    print(limits)
+    limits = np.array(pd.read_parquet(limitsPath))[-1,-1]
+    values = np.array(pd.read_parquet(valuesPath))[-1,-1]
 
-    print(values)
-    print(np.shape(values))
-    #fig, ax = plt.subplots()
+    for i in range(0, len(limits), 2):
+        print(f"Variable {i//2 + 1} limits: ({limits[i]}, {limits[i+1]})")
 
-    fig, axs = plt.subplots(2, 2)
+    fig, axs = plt.subplots(1, 3,figsize=(12,4))
 
     length = int(len(values) / 3)
 
@@ -86,25 +76,25 @@ def scatterPlot(dimensions, alph, limitsPath, samplePath):
         for j in range(length):
             xs[i,j] = values[3*j + i]
 
-    axs[0,0].scatter(xs[0,], xs[1,], alpha = alph)
-    axs[0, 0].set_title('X2,X4')
-    #axs[0,0].set_xlabel('X2')
-    #axs[0,0].set_ylabel('X4')
-    axs[0,1].scatter(xs[0,], xs[2,], alpha = alph)
-    axs[0, 1].set_title('X2,X5')
-    #axs[0,1].set_xlabel('X2')
-    #axs[0,1].set_ylabel('X5')
-    axs[1,1].scatter(xs[1,], xs[2,], alpha = alph)
-    axs[1, 1].set_xlabel('X4,X5')
-    #axs[1,1].set_xlabel('X4')
-    #axs[1,1].set_ylabel('X5')
+    # Assuming `color_values` is a list of colors for each point
+    color_values = np.random.rand(length)  # Example random color values
 
-    # Customize the z axis.
-    #ax.set_xlim(x4_min, x4_max)
-    #ax.set_ylim(x6_min, x6_max)
-    #ax.set_xlabel('X1')
-    #ax.set_ylabel('X2')
+    # Scatter plots with color values and colorbar
+    sc0 = axs[0].scatter(xs[0, :], xs[1, :], c=color_values, alpha=alph)
+    axs[0].set_title('X2,X4')
+    cbar0 = fig.colorbar(sc0, ax=axs[0])
+    cbar0.set_label('f_n(X2,X4)')
+
+    sc1 = axs[1].scatter(xs[0, :], xs[2, :], c=color_values, alpha=alph)
+    axs[1].set_title('X2,X5')
+    cbar1 = fig.colorbar(sc1, ax=axs[1])
+    cbar1.set_label('f_n(X2,X5)')
+
+    sc2 = axs[2].scatter(xs[1, :], xs[2, :], c=color_values, alpha=alph)
+    axs[2].set_title('X4,X5')
+    cbar2 = fig.colorbar(sc2, ax=axs[2])
+    cbar2.set_label('f_n(X4,X5)')
 
     plt.show()
 
-#scatterPlot(3, 1, limitsPath, samplePath)
+scatterPlot(3, 1, limitsPath, samplePath)
